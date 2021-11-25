fe496864137144fca3c3d41d60f63c542b149e03
Тихомир Найденов <tihomirnaydenov02@pmgkk.com>
2021-11-25 16:47:02 +0200

    Add `Minions` exercise.

---
+++ Database Minions.sql
@@ -0,0 +1,95 @@
+CREATE DATABASE MinionsDB
+USE MinionsDB
+
+CREATE TABLE Minions (
+	Id INT PRIMARY KEY,
+	Name NVARCHAR(100),
+	Age INT
+)
+
+CREATE TABLE Towns (
+	Id INT PRIMARY KEY,
+	Name NVARCHAR(120)
+)
+
+ALTER TABLE Minions
+ADD TownId INT
+
+ALTER TABLE Minions
+ADD FOREIGN KEY (TownId) REFERENCES Towns(Id)
+
+INSERT INTO Towns (Id, Name) VALUES
+(1, 'Sofia'),
+(2, 'Plovdiv'),
+(3, 'Varna')
+
+INSERT INTO Minions (Id, Name, Age, TownId) VALUES
+(1, 'Kevin', 22, 1),
+(2, 'Bob', 15, 3),
+(3, 'Steward', NULL, 2)
+
+TRUNCATE TABLE Minions
+
+DROP TABLE Minions
+DROP TABLE Towns
+
+CREATE TABLE People (
+	Id INT PRIMARY KEY IDENTITY,
+	Name NVARCHAR(200) NOT NULL,
+	Picture VARBINARY(MAX) CHECK (DATALENGTH(Picture) > 1024 * 1024 * 2),
+	Height DECIMAL(3, 2),
+	[Weight] DECIMAL (5, 2),
+	Gender CHAR(1) CHECK (Gender = 'm' OR Gender = 'f') NOT NULL,
+	Birthdate DATE NOT NULL,
+	Biography NVARCHAR(MAX)
+)
+
+INSERT INTO People (Name, Picture, Height, [Weight], Gender, Birthdate, Biography) VALUES
+('Pesho Georgiev', NULL, 1.80, 55.23, 'm', Convert(DateTime,'19820626',112), 'IT'),
+('Ivan Dimov', NULL, 1.75, 75.55, 'm', Convert(DateTime,'19850608',112), 'Basketball Player'),
+('Todorka Peneva', NULL, 1.66, 48.55, 'f', Convert(DateTime,'19900606',112), 'Model'),
+('Ivan Hristov', NULL, 1.77, 52.22, 'f', Convert(DateTime,'19920705',112), 'Football Player'),
+('Todor Stoilov', NULL, 1.88, 98.25, 'm', Convert(DateTime,'19870706',112), 'Lawyer')
+
+CREATE TABLE Users (
+	Id BIGINT UNIQUE IDENTITY,
+	Username VARCHAR(30) UNIQUE NOT NULL,
+	Password VARCHAR(26) NOT NULL,
+	ProfilePicture VARBINARY(MAX) CHECK (DATALENGTH(ProfilePicture) <= 900 * 1024),
+	LastLoginTime DATETIME,
+	IsDeleted BIT
+	CONSTRAINT PK_Users PRIMARY KEY(Id)
+)
+
+INSERT INTO Users VALUES
+('Pesho', '12345', NULL, NULL, 0),
+('Gosho', '12345', NULL, NULL, 1),
+('Ivan', '12345', NULL, NULL, 0),
+('Kamen', '12345', NULL, NULL, 0),
+('Dancho', '12345', NULL, NULL, 1)
+
+ALTER TABLE Users
+DROP CONSTRAINT PK_Users
+
+ALTER TABLE Users
+ADD CONSTRAINT PK_Users PRIMARY KEY(Id, Username)
+
+ALTER TABLE Users
+ADD CONSTRAINT PasswordLength CHECK (LEN(Password) >= 5)
+
+ALTER TABLE Users
+ADD DEFAULT GETDATE() FOR LastLoginTime
+
+ALTER TABLE Users
+DROP CONSTRAINT PK_Users
+
+ALTER TABLE Users
+ADD CONSTRAINT PK_Id
+PRIMARY KEY (Id)
+
+ALTER TABLE Users
+ADD CONSTRAINT uq_Username
+UNIQUE(Username)
+
+ALTER TABLE Users
+ADD CONSTRAINT UsernameLength CHECK (LEN(Username) >= 3)
